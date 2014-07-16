070a7b633d6676a957e375c4d007ee7a33e9af0f
Justin Nicholson <jwnicholson14@gmail.com>
2014-07-16 15:36:29 -0400

    Rough draft of consistency check code
    Extremely rough consistency check function written.

--- script/consistency.R
+++ script/consistency.R
@@ -16,7 +16,73 @@ if (length(obsdel) > 0){
   cat("These observations have been dropped in new data:", obsdel, sep = '\n')
 }else if(length(obsadd) > 0){
   cat("These observations have been added in new data:", obsadd, sep = '\n')
-}else cat("Observations match")
+}else {cat("Observations match")
 }
 
+#code for proper subsetting should go here
+#change hardcoded backup / suspect to whichever is longer dynamically.
+rowinter = intersect(as.vector(suspect.frame$CASE_ID), as.vector(backup.frame$CASE_ID))
+
+#Row subset
+backup.subset = backup.frame[backup.frame$CASE_ID %in% inter,]
+
 #COLUMN CONSISTENCY
+coldel = setdiff(as.vector(colnames(backup.frame)), as.vector(colnames(suspect.frame)))
+coladd = setdiff(as.vector(colnames(suspect.frame)), as.vector(colnames(backup.frame)))
+colinter =as.matrix(intersect(as.vector(colnames(suspect.frame)), as.vector(colnames(backup.frame))))
+
+if (length(coldel) > 0){
+  cat("These variables have been dropped or changed in new data:", coldel, sep = '\n')
+}else if(length(coladd) > 0){
+  cat("These variables have been added or changed in new data:", coladd, sep = '\n')
+}else {cat("variable names match")
+}
+
+#Column subset
+suspect.subset = suspect.frame[,colnames(suspect.frame) %in% colinter]
+backup.subset = backup.subset[,colnames(backup.subset) %in% colinter]
+#DATA CONSISTENCY
+#change hard coded 'colinter' to dynamically changing variable. Also check colinter call
+sordered = suspect.subset[order(suspect.subset$CASE_ID), ]
+bordered = backup.subset[order(backup.subset$CASE_ID), ]
+
+
+for (i in 1:nrow(colinter)){
+  oldloc = match(colinter[i], colnames(backup.subset))
+  newloc = match(colinter[i], colnames(suspect.subset))
+
+ if (any(is.na(sordered)) == F & any(is.na(bordered)) == F){
+  if(prod(as.vector(sordered[,newloc]) == as.vector(bordered[,oldloc])) == 1){
+cat("Column",i, "matches", '\n')
+  } else{
+    cat("Column",colnames(colinter[i]), "does not match. Location saved")
+    inconsistent = append(inconsistent, colinter[i])
+}
+
+}else{
+  sorderedmod = sordered
+  sorderedmod = sorderedmod[which(is.na(sorderedmod[,newloc])), newloc] == c("NA")
+  borderedmod = bordered
+  borderedmod = borderedmod[which(is.na(borderedmod[,oldloc])), oldloc] == c("NA")
+  if(prod(toString(sordered[,newloc]) == toString(bordered[,oldloc])) == 1){
+    cat("Column",i, "matches", '\n')
+    }else{
+      cat("Column",i, "does not match. Location saved", '\n')
+   inconsistent = cbind(inconsistent, c(colinter[i]),newloc, oldloc, i)
+     }
+}
+}
+
+#SHOW INCONSISTENCIES
+for(j in 1:nrow(inconsistent)){
+show = as.vector(which(sordered[,as.integer(inconsistent[j,2])] != bordered[,as.integer(inconsistent[j,3])]))
+for (k in 1:NROW(show)){
+cat("row", show[k] ,'\n', "difference in variable", inconsistent[j,1], '\n',
+    toString(sordered[show[k],as.integer(inconsistent[j,2])]),
+    "v.s.", 
+    toString(bordered[show[k],as.integer(inconsistent[j,3])]), '\n \n')
+}
+}
+
+#CLEANUP
+rm(obsdel, obsadd, coldel, coladd, colinter, backup.subset, data.subset, suspect.subset, borderedmod, i, k, inter, newloc, oldloc, sorderedmod, bordered)
