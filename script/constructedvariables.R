122d3ef4b409e306ca63c69159886bc331bb2744
Justin Nicholson <jwnicholson14@gmail.com>
2014-07-17 17:04:49 -0400

    Cleaning data for Archigos
    Removed flawed tenure variable, created csv for easier viewing of data.

--- script/constructedvariables.R
+++ script/constructedvariables.R
@@ -36,20 +36,3 @@ arch$ccode[arch$idacr == "GFR"] = 255
 
 #save the final product
 save.image(file = "adjudication.rda")
-
-#ROW CONSISTENCY
-#Check for observation consistency between sets based on user set unique key. 
-#Right now this is limited to one variable.
-obsdel = setdiff(as.vector(backup.frame$CASE_ID), as.vector(suspect.frame$CASE_ID))
-obsadd = setdiff(as.vector(suspect.frame$CASE_ID), as.vector(backup.frame$CASE_ID))
-if (length(obsdel) > 0){
-  cat("These observations have been dropped in new data:", obsdel, sep = '\n')
-}else if(length(obsadd) > 0){
-    cat("These observations have been added in new data:", obsadd, sep = '\n')
-}else cat("Observations match")
-  }
-
-#COLUMN CONSISTENCY
-
-#difference in sets
-setdiff(as.vector(backup.frame$CASE_ID), as.vector(suspect.frame$CASE_ID))
