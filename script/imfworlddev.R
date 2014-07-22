835e131d1796ace677c5068e8144c3ed41bf6799
Justin Nicholson <jwnicholson14@gmail.com>
2014-07-22 16:49:17 -0400

    End of day save

--- script/imfworlddev.R
+++ script/imfworlddev.R
@@ -96,3 +96,4 @@ data$t.ccode[data$INV_CTY_NAME == "Belarus" & data$einitdate <= '1991-08-25'] = 
 data$t.ccode[data$INV_CTY_NAME == "Macedonia" & data$einitdate <= '1993-04-8'] = 365
 data$t.ccode[data$INV_CTY_NAME == "Ukraine" & data$einitdate <= '1991-08-24'] = 365
 data$t.ccode[data$INV_CTY_NAME == "Slovakia" & data$einitdate <= '1993-12-01'] = 315
+data$t.ccode[data$INV_CTY_NAME == "Bosnia Herzegovina" & data$einitdate <= '1992-03-01'] = 345
